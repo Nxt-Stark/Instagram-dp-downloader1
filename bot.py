f65d330a58a26e6fc52635242b8f9b9b97ab6888
NxtStark <85378792+Nxt-Stark@users.noreply.github.com>
2023-07-15 15:26:09 +0630

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,7 +5,6 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from handlers.broadcast import broadcast
 from handlers.check_user import handle_user_status
 from handlers.database import Database
-from handlers.database.users_chats_db import db
 from utils import temp
 
 logging.basicConfig(
