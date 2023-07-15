2cbadbf02eacec9ed20e36f49553341a265b381b
NxtStark <85378792+Nxt-Stark@users.noreply.github.com>
2023-07-15 12:04:39 +0630

    Create info.py

---
+++ info.py
@@ -0,0 +1,8 @@
+import re
+from os import environ
+
+API_ID = os.getenv("API_ID")
+API_HASH = os.getenv("API_HASH")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+APP_NAME = os.getenv("APP_NAME")
+TELEGRAM_USERNAME = os.getenv("TELEGRAM_USERNAME")
