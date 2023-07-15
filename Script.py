859d634cb976ef280a1b68834d0e2bdd35fcfb2d
NxtStark <85378792+Nxt-Stark@users.noreply.github.com>
2023-07-15 13:13:11 +0630

    Create Script.py

---
+++ Script.py
@@ -0,0 +1,54 @@
+class script(object):
+    START_TXT = """Hi {} Welcome To Mr.InstDpHTM_Bot 🖐🖐.
+
+Send anyones instagram username to get their DP 🍂.
+
+Click /help to More info 💫.
+ 
+Eg: nxt.stark , h_tech_media etc
+ 
+ 
+Made With ❤️ By <a href=https://t.me/HTechMedia>HTechMedia</a>"""
+    OWNER_TXT = """<b>⍟───[ ᴏᴡɴᴇʀ ᴅᴇᴛᴀɪʟꜱ ]───⍟</b>
+    
+<b>• ꜰᴜʟʟ ɴᴀᴍᴇ : ᴺˣᵀˢᵀᴬᴿᴷ</b>
+<b>• ᴜꜱᴇʀɴᴀᴍᴇ : @NxtStark</b>
+<b>• ᴘᴇʀᴍᴀɴᴇɴᴛ ᴅᴍ ʟɪɴᴋ : <a href=https://telegram.dog/NxtStark>ᴄʟɪᴄᴋ ʜᴇʀᴇ</a></b>
+<b>“му нєαят'ѕ α ѕтєяєσ. ιт вєαтѕ fσя уσυ, ѕσ ℓιѕтєи ¢ℓσѕє”</b>"""  
+
+    HELP_TXT = """<b>Hᴇʏ {} Fʀɪᴇɴᴅ Hᴇʀᴇ Yᴏᴜʀ Bᴜᴛᴛᴏɴs 👇</b>"""
+
+
+    ABOUT_TXT = """<b>🤖 ᴍʏ ɴᴀᴍᴇ : <a href=https://t.me/Tamilanz_Auto_Filter_Bot>ᴍᴏᴠɪᴇs ғɪʟᴛᴇʀ ʙᴏᴛ</a>
+👨‍💻 ᴅᴇᴠᴇʟᴏᴘᴇʀ : <a href=https://t.me/Sharathitsisme>ᴺˣᵀˢᵀᴬᴿᴷ</a>
+📝 ʟᴀɴɢᴜᴀɢᴇ : ᴘʏʀᴏɢʀᴀᴍ
+📚 ꜰʀᴀᴍᴇᴡᴏʀᴋ : ᴘʏᴛʜᴏɴ 3
+📡 ʜᴏsᴛᴇᴅ ᴏɴ : ғʀᴇᴇ ʜᴏsᴛɪɴɢ
+📢 sᴜᴘᴘᴏʀᴛ ɢʀᴏᴜᴘ : <a href=https://t.me/HTechMediaSupport>ᴄʟɪᴄᴋ ʜᴇʀᴇ</a>
+🌟 Sᴏᴜʀᴄᴇ Cᴏᴅᴇ : <a href=https://github.com/NxtStark/Instagram-dp-downloader>ʀᴇᴩᴏ ʟɪɴᴋ</a></b>"""
+
+
+    SOURCE_TXT = """Tʜɪs Is Aɴ Oᴩᴇɴ-Sᴏᴜʀᴄᴇ Pʀᴏᴊᴇᴄᴛ Bʏ @Nxt-Stark</b>
+
+- 100﹪ Cᴏᴅᴇᴅ Bʏ <a href=https://t.me/NxtStark>ᴺˣᵀˢᵀᴬᴿᴷ</a></b>
+
+- Rᴇᴩᴏ Lɪɴᴋ 👇 Hᴇʀᴇ"""
+
+
+    CONTACT_TXT = """<b>
+<b>»» ° Oɴʟʏ Cᴏɴᴛᴀᴄᴛ Fᴏʀ Pᴀɪᴅ Wᴏʀᴋs / Pʀᴏʙʟᴇᴍ / Dᴏᴜʙᴛ / Cᴏʟʟᴀʙ / Hᴇʟᴩ °</b>
+
+<b>»» Iғ U Cᴏɴᴛᴀᴄᴛ Mᴇ Sᴇᴇ Bᴇʟᴏᴡ Bᴜᴛᴛᴏɴs 😙</b>
+"""
+
+    STATUS_TXT = """<b><u>Cᴜʀʀᴇɴᴛ Dᴀᴛᴀʙᴀsᴇ Sᴛᴀᴛᴜs</b></u>
+    
+<b>👩🏻‍💻 ᴜsᴇʀs: <code>{}</code>
+
+🗂️ Notification enabled: <code>{}</code>
+"""
+
+    LOG_TEXT_P = """<b> #NewUser
+ɪᴅ ♥️- <code>{}</code>
+ɴᴀᴍᴇ 💥- {} </b>
+"""
