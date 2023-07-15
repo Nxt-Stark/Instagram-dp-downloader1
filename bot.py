db4ac3145486d170ece574b07f0b94a1464543c5
NxtStark <85378792+Nxt-Stark@users.noreply.github.com>
2023-07-15 15:30:00 +0630

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,11 +1,9 @@
 import logging
-import os
 from pyrogram import Client, __version__, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from handlers.broadcast import broadcast
 from handlers.check_user import handle_user_status
 from handlers.database import Database
-from utils import temp
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -25,14 +23,8 @@ class Bot(Client):
         )
 
     async def start(self):
-        b_users, b_chats = await db.get_banned()
-        temp.BANNED_USERS = b_users
-        temp.BANNED_CHATS = b_chats
         await super().start()
         me = await self.get_me()
-        temp.ME = me.id
-        temp.U_NAME = me.username
-        temp.B_NAME = me.first_name
         self.username = "@" + me.username
         logging.info(f"{me.first_name} with for Pyrogram v{__version__} (Layer {layer}) started on {me.username}.")
         logging.info(LOG_STR)
@@ -41,7 +33,7 @@ class Bot(Client):
         await super().stop()
         logging.info("Bot stopped. Bye.")
 
-
 app = Bot()
 app.run()
+
 
