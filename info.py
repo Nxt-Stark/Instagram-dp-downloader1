17022283ce5f48f46b69bf9cb2152059965e2955
NxtStark <85378792+Nxt-Stark@users.noreply.github.com>
2023-07-15 13:01:43 +0630

    Update info.py

--- info.py
+++ info.py
@@ -1,11 +1,13 @@
 import re
+import os
 from os import environ
 
-API_ID = os.getenv("API_ID")
-API_HASH = os.getenv("API_HASH")
-BOT_TOKEN = os.getenv("BOT_TOKEN")
+API_ID = int(os.environ.get("API_ID", ""))
+API_HASH = os.environ.get("API_HASH", "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 APP_NAME = os.getenv("APP_NAME")
 TELEGRAM_USERNAME = os.getenv("TELEGRAM_USERNAME")
-DB_URL = os.getenv("DB_URL")
-DB_NAME = os.getenv("DB_NAME")
-LOG_CHANNEL = os.getenv("LOG_CHANNEL")
+DB_URL = os.environ.get("DB_URL", "")
+DB_NAME = os.environ.get("DB_NAME", "BroadcastBot")
+LOG_CHANNEL = os.environ.get("LOG_CHANNEL", "123")
+BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", True))
