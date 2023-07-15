a2789596c43ef58c11690478a57b5a719b4d3c08
NxtStark <85378792+Nxt-Stark@users.noreply.github.com>
2023-07-15 15:38:56 +0630

    Update info.py

--- info.py
+++ info.py
@@ -1,7 +1,7 @@
 import re
 import os
 from os import environ
-
+SESSION = environ.get('SESSION', 'Media_search')
 API_ID = int(os.environ.get("API_ID", ""))
 API_HASH = os.environ.get("API_HASH", "")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
